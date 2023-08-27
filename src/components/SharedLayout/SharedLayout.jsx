e2987b564191d177090bd691933d9def1d1c05f8
MateuszBieda <mateusbieda@gmail.com>
2023-08-27 12:39:42 +0200

    links update

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -8,8 +8,8 @@ export const SharedLayout =()=>{
         <Container>
           <Header>
             <nav>
-              <Link to="/">Home</Link>
-              <Link to="/movies">Movies</Link>
+              <Link to="/goit-react-hw-05-movies/">Home</Link>
+              <Link to="/goit-react-hw-05-movies//movies">Movies</Link>
             </nav>
           </Header>
       <Suspense fallback={<div>Loading page...</div>}>
